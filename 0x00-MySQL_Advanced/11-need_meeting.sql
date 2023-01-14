12eb3a8fee65c954e1e2743bc7f5aac2309ecaf4
tommydebisi <tommydebisi@gmail.com>
2023-01-14 16:06:47 +0100

    :construction: task 11 - use of view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,10 @@
+-- SQL script that creates a view need_meeting that lists all students that have a
+-- score under 80 (strict) and no last_meeting or more than 1 month.
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80 AND (
+                        last_meeting <=> NULL OR
+                        CURDATE() > DATE_ADD(last_meeting, INTERVAL 1 MONTH)
+                    )
