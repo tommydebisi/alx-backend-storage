68cb8f97d1333dd3ed9560188449761f6ac51d96
tommydebisi <tommydebisi@gmail.com>
2023-01-14 21:41:30 +0100

    :ssedling; updated variables

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,12 +1,11 @@
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
 -- that computes and store the average weighted score for a student.
 
--- DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     DECLARE pro_id, scor, weighted, done_l INT DEFAULT 0;
-    DECLARE weigh_score, avg_weighted, weigh_sum INT DEFAULT 0;
+    DECLARE weigh_score, weigh_sum INT DEFAULT 0;
 
     DECLARE curs_1 CURSOR FOR
     SELECT project_id, score FROM corrections
@@ -16,30 +15,27 @@ BEGIN
 
     OPEN curs_1;
 
-    getAvgWeight: LOOP
+    REPEAT
         -- FETCH values in each row and store them
         FETCH NEXT FROM curs_1 INTO pro_id, scor;
 
-        IF (done_l = 1)
+        IF NOT done_l
         THEN
-            LEAVE getAvgWeight;
+            -- get weighted value
+            SELECT projects.weight INTO weighted
+            FROM projects
+            WHERE projects.id = pro_id;
+
+            -- get summation of the product weighted val and score
+            SET weigh_score = weigh_score + (weighted * scor);
+
+            -- get summation of initial weighted val and current weighted val
+            SET weigh_sum = weigh_sum + weighted;
         END IF;
-        -- get weighted value
-        SELECT projects.weight INTO weighted
-        FROM projects
-        WHERE projects.id = pro_id;
-
-        -- get summation of the product weighted val and score
-        SET weigh_score = weigh_score + (weighted * scor);
-
-        -- get summation of initial weighted val and current weighted val
-        SET weigh_sum = weigh_sum + weighted;
-    END LOOP getAvgWeight;
-
-    SET avg_weighted = (weigh_score / weigh_sum);
+    UNTIL done_l END REPEAT;
 
     UPDATE users
-    SET average_score = avg_weighted
+    SET average_score = (weigh_score / weigh_sum)
     WHERE users.id = user_id;
 
     CLOSE curs_1;
