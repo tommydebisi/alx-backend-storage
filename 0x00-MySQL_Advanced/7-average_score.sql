2a7098989c0fce190242fe0b1d37c7c49ecf5c2c
tommydebisi <tommydebisi@gmail.com>
2023-01-14 12:58:40 +0100

    :seedling: used @ to declare user var

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,15 +6,14 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 0);
 
-    SELECT AVG(score) INTO avg_score
+    SELECT AVG(score) INTO @avg_score
     FROM corrections
     WHERE corrections.user_id = user_id
     GROUP BY corrections.user_id;
 
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = @avg_score
     WHERE users.id = user_id;
 END $$
 DELIMITER ;
