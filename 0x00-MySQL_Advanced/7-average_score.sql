df287f7782fc717bac249da05de1cec3b6fa119f
tommydebisi <tommydebisi@gmail.com>
2023-01-14 12:44:56 +0100

    :construction: use of avg in stored procedure

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+--  SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and
+-- store the average score for a student. Note: An average score can be a decimal
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score DECIMAL(10, 0);
+
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE corrections.user_id = user_id
+    GROUP BY corrections.user_id;
+
+    UPDATE users
+    SET average_score = avg_score
+    WHERE users.id = user_id;
+END $$
+DELIMITER ;
