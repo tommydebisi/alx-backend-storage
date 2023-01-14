d16b7669a5157c3702652368ead3f17ced9ed5e9
tommydebisi <tommydebisi@gmail.com>
2023-01-14 12:04:35 +0100

    :construction: use of variables & stored procedure

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,19 @@
+-- SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE IF NOT EXISTS AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE proid INT;
+
+    IF ((SELECT id FROM projects WHERE name = project_name) <=> NULL)
+    THEN
+        INSERT INTO projects (projects.name) VALUES(project_name);
+    END IF;
+
+    SELECT id INTO proid FROM projects WHERE name = project_name;
+
+    INSERT INTO corrections (corrections.user_id, project_id, score)
+    VALUES(user_id, proid, score);
+END $$
+DELIMITER ;
