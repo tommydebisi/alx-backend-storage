5d58570225fc1b845d607479d688d3c740e911f5
tommydebisi <tommydebisi@gmail.com>
2023-01-14 23:15:42 +0100

    :seedling: used the IF function

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- SQL script that lists all bands with Glam rock as their main style,
 -- ranked by their longevity
 
-SELECT band_name, (IFNULL(split, NOW()) - formed) as lifespan
+SELECT band_name, (IF(split <=> NULL, 2022, split) - formed) as lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
