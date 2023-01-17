b199a8c070ad7c33d7fa729598f0472854236948
tommydebisi <tommydebisi@gmail.com>
2023-01-17 22:55:20 +0100

    :seedling: check if name or topics is empty

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,6 +9,9 @@ def update_topics(mongo_collection, name, topics):
         This function changes all topics of a school
         document based on the name given
     """
+    if not name or not topics:
+        return
+
     mongo_collection.update_one({'name': name}, {
         '$set': {'topics': topics}
         }, upsert=True)
