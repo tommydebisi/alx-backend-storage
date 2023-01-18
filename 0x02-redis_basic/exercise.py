6fc1292d6ef8676c67c8689bcd30b9209164a422
tommydebisi <tommydebisi@gmail.com>
2023-01-19 00:40:10 +0100

    :seedling: implemented get_str/int

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,10 +3,11 @@
     exercise mod
 """
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 import uuid
 
 
+ReturnData = Union[bytes, str, int, float]
 class Cache:
     """
         Class that caches input to redis server
@@ -14,14 +15,29 @@ class Cache:
     def __init__(self) -> None:
         """ constructor function """
         self._redis = redis.Redis()
-        self._redis.flushdb(asynchronous=True)
+        self._redis.flushdb()
 
-    def store(self, data: Union[bytes, str, int, float]) -> str:
+    def store(self, data: ReturnData) -> str:
         """
             takes a data storing it with a unique id and returning
             the key
         """
-        if data:
-            key = uuid.uuid4()
-            self._redis.set(str(key), data)
-            return str(key)
+
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable]=None) -> ReturnData:
+        """
+            returns the value goten from the key in database
+        """
+        val = self._redis.get(key)
+        return fn(val) if fn else val
+
+    def get_str(self, key: str) -> str:
+        """ returns a string value from database """
+        return self.get(key, lambda d: d.encode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """ returns a int value from database """
+        return self.get(key, int)
