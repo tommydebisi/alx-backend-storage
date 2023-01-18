871e4f96db38e0062ec175754f809bee5d289a77
tommydebisi <tommydebisi@gmail.com>
2023-01-18 19:05:28 +0100

    :construction: writing string to redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    exercise mod
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """
+        Class that caches input to redis server
+    """
+    def __init__(self) -> None:
+        """ constructor function """
+        self._redis = redis.Redis()
+        self._redis.flushdb(asynchronous=True)
+
+    def store(self, data: Union[bytes, str, int, float]) -> str:
+        """
+            takes a data storing it with a unique id and returning
+            the key
+        """
+        key = uuid.uuid4()
+        self._redis.set(str(key), data)
+        return str(key)
