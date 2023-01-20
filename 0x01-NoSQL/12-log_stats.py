60ade273068fe3ffc5ba7607a852c7e9541e328e
tommydebisi <tommydebisi@gmail.com>
2023-01-20 13:11:34 +0100

    :seedling: used count documents for all

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -11,21 +11,15 @@ if __name__ == '__main__':
 
     print("{} logs".format(collection.count_documents({})))
     print("Methods:")
-    print("\tmethod GET: {}".format(len(list(collection.find({
-        "method": {'$in': ["GET"]}
-        })))))
-    print("\tmethod POST: {}".format(len(list(collection.find({
-        "method": {'$in': ["POST"]}
-        })))))
-    print("\tmethod PUT: {}".format(len(list(collection.find({
-        "method": {'$in': ["PUT"]}
-        })))))
-    print("\tmethod PATCH: {}".format(len(list(collection.find({
-        "method": {'$in': ["PATCH"]}
-        })))))
-    print("\tmethod DELETE: {}".format(len(list(collection.find({
-        "method": {'$in': ["DELETE"]}
-        })))))
-    print("{} status check".format(len(list(collection.find({
-        "path": {'$in': ["/status"]}
-        })))))
+    print("\tmethod GET: {}".format(collection.count_documents(
+                                    {"method": "GET"})))
+    print("\tmethod POST: {}".format(collection.count_documents(
+                                    {"method": "POST"})))
+    print("\tmethod PUT: {}".format(collection.count_documents(
+                                    {"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(collection.count_documents(
+                                    {"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(collection.count_documents(
+                                    {"method": "DELETE"})))
+    print("{} status check".format(collection.count_documents(
+                                    {"path": "/status"})))
